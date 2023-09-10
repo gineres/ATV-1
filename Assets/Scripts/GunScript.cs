76a7b23a9ff3019c9b53e2e18bfeaf09969713cc
gineres <giselle.neres.contact@gmail.com>
2023-09-10 18:50:32 -0300

    Auto script done

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -5,22 +5,67 @@ using UnityEngine.UI;
 
 public class GunScript : MonoBehaviour
 {
-    //public Carregador carregadorArma = Carregador.Pistol;
     public CarregadorScript carregador;
     public GameObject bbObject;
     public Transform bbFirePos;
-
     int currentBullets;
-    float rof;
 
-    [Header ("UI Handling")]
+    [Header("UI Handling")]
     public Text bulletsText;
+
+    private float shootTimer; // Timer for shooting delay
+    private bool isShooting; // Flag to control shooting delay
+    private bool autoMode = false;
 
     void Update()
     {
         currentBullets = carregador.GetCurrentBullets();
         UpdateUI();
-        Fire();
+
+        // Check if the gun is currently shooting and the delay has passed
+        if (isShooting)
+        {
+            shootTimer -= Time.deltaTime;
+            if (shootTimer <= 0f)
+            {
+                isShooting = false;
+            }
+        }
+
+        // If the fire button is held down and there are bullets, shoot continuously
+        if (autoMode)
+        {
+            if (Input.GetMouseButton(0) && currentBullets > 0 && !isShooting)
+            {
+                CallShooting();
+            }   
+        } else
+        {
+            if (Input.GetMouseButtonDown(0) && currentBullets > 0 && !isShooting)
+            {
+                CallShooting();
+            } 
+        }
+    }
+
+    void CallShooting()
+    {
+        // Set the shooting delay based on the carregador type
+        if (carregador.GetTipoCarregador() == Carregador.Pistol)
+        {
+            shootTimer = .2f; // Adjust this value as needed
+        }
+        else if (carregador.GetTipoCarregador() == Carregador.Shotgun)
+        {
+            shootTimer = 0.13f; // Adjust this value as needed
+        }
+        else
+        {
+            shootTimer = 0.05f; // Adjust this value as needed
+        }
+
+        isShooting = true;
+        Invoke("Shoot", shootTimer);
     }
 
     void UpdateUI()
@@ -28,14 +73,19 @@ public class GunScript : MonoBehaviour
         bulletsText.text = currentBullets.ToString();
     }
 
-    void Fire()
+    void Shoot()
     {
-        if (Input.GetMouseButtonDown(0) && currentBullets != 0)
-        {
-            GameObject bb = Instantiate(bbObject, bbFirePos.position, bbFirePos.rotation) as GameObject;
-            Ball bbScript = bb.GetComponent<Ball>();
-            bbScript.AdjustBallSettings(1.2f, carregador.GetBulletWeight());
-            carregador.SetCurrentBullets(currentBullets - 1);
-        }
+        GameObject bb = Instantiate(bbObject, bbFirePos.position, bbFirePos.rotation) as GameObject;
+        Ball bbScript = bb.GetComponent<Ball>();
+        bbScript.AdjustBallSettings(1.2f, carregador.GetBulletWeight());
+        carregador.SetCurrentBullets(currentBullets - 1);
+    }
+
+    public void TurnAutoOn(){
+        autoMode = true;
+    }
+
+    public void TurnAutoOff(){
+        autoMode = false;
     }
 }
--- Assets/Scripts/PlayerActions.cs
+++ Assets/Scripts/PlayerActions.cs
@@ -5,18 +5,7 @@ using UnityEngine;
 public class PlayerActions : MonoBehaviour
 {
     public GunScript gunScript;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     void OnCollisionEnter(Collision other)
     {
         Debug.Log("ENTROU");
@@ -26,16 +15,16 @@ public class PlayerActions : MonoBehaviour
             if (gunScript.carregador.GetTipoCarregador() == mag.magType)
             {
                 gunScript.carregador.SetTipoCarregador(mag.magType); // colocar pra ser flexivel dps
-                Destroy(other.gameObject);
+                Destroy(other.gameObject.transform.parent.gameObject);
             } else
             {
-                Debug.Log("NÃO COMPATÍVEL!!!");
+                Debug.Log("NAO COMPATIVEL!!!");
             }
         } else if (other.gameObject.tag == "Gun")
         {
             CollectibleGun gun = other.gameObject.GetComponent<CollectibleGun>() as CollectibleGun;
             gunScript.carregador.SetTipoCarregador(gun.gunType); // colocar pra ser flexivel dps
-            Destroy(other.gameObject);
+            Destroy(other.gameObject.transform.parent.gameObject);
         }
     }
 }
